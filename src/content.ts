def9259b83898c945f994d6e720d26b1853f0c8e
Phillip Nguyen <phillip.d.nguyen23@gmail.com>
2020-12-21 21:48:05 -0800

    feat: began creating term defintion popup ui. code is still a bit messy

--- src/content.ts
+++ src/content.ts
@@ -1,16 +1,41 @@
 // This file is injected as a content script
-console.log("Hello from content script!");
-const KEY = process.env.DICT_KEY;
+const { DICT_KEY } = process.env;
 const BASE_URL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json`;
 
 document.addEventListener("dblclick", async (event) => {
-    const selectedText = window.getSelection()?.toString();
+  const selectedText = window.getSelection()?.toString();
+  const anchoredNode = window.getSelection()?.anchorNode;
 
-    if (selectedText?.trim() === "") return;
+  document.querySelector(".term-definition")?.remove();
 
-    let url = `${BASE_URL}/${encodeURI(selectedText?.trim() as string)}?key=${KEY}`;
+  if (selectedText?.trim() === "") return;
 
-    const results = await fetch(url);
-    const data = await results.json();
-    console.log(data);
-})
+  const url = `${BASE_URL}/${encodeURI(
+    selectedText?.trim() as string
+  )}?key=${DICT_KEY}`;
+
+  const results = await fetch(url);
+  const data = await results.json();
+  console.log(data);
+
+  const scrollTop = window.pageYOffset
+    ? window.pageYOffset
+    : (document.documentElement || document.body.parentNode || document.body)
+        .scrollTop;
+
+  // Get cursor position;
+  const posX = event.clientX;
+  const postY = event.clientY + scrollTop;
+  console.log(`Mouse Y: ${event.clientY} - ScrollTop: ${scrollTop}`);
+  const html = `
+    <div class="term-definition" style="height: 50px; width: 100px; background: #000; color: #FFF; position: absolute; top:${postY}; left:${posX}; ">
+      <p>Here lies the term definition</p>
+    </div>
+    `;
+  const parser = new DOMParser();
+  const newNode = parser
+    .parseFromString(html, "text/html")
+    .querySelector(".term-definition");
+
+  anchoredNode?.parentElement?.insertBefore(newNode as Node, null);
+});
